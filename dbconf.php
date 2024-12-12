e07c576bed42de466ff0642c08a47285c11c95ad
AloneSweety <139615872+AloneSweety@users.noreply.github.com>
2024-12-12 18:09:07 +0000

    edit

--- dbconf.php
+++ dbconf.php
@@ -5,7 +5,7 @@ $password = "mariadb";            // your DB password
 $dbname = "chrono_explorer"; // your DB name
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$connect = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
